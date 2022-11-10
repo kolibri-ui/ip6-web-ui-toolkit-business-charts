fb2c93c5e768466243dffc540db6fa4e2c7ccaba
valentina <valentina.giampa@students.fhnw.ch>
2022-11-10 11:41:01 +0100

    red: from down right to up left, new point: down left

--- src/business-charts/util/chartTest.js
+++ src/business-charts/util/chartTest.js
@@ -258,4 +258,23 @@ chartSuite.add("from down right to up left, new point: up right",
         assert.is(newPoint.newXPos, 510);
         assert.is(newPoint.newYPos, -10);
     });
+
+chartSuite.add("from down right to up left, new point: down left",
+    assert => {
+        const newPoint = getNewPointPos(
+            "CORNER_DOWN_RIGHT",
+            "CORNER_UP_LEFT",
+            520,
+            -520,
+            "CORNER_DOWN_LEFT",
+            500,
+            500,
+            10,
+            10
+        );
+
+        assert.is(newPoint.newXPos, 10);
+        assert.is(newPoint.newYPos, -510);
+    });
+
 chartSuite.run();
