20709782deef6460f66dda1b7dd9e3e174c3cdec
valentina <valentina.giampa@students.fhnw.ch>
2022-11-14 13:22:17 +0100

    chore: structure of test imports

--- src/business-charts/allBusinessChartsTestsSuite.js
+++ src/business-charts/allBusinessChartsTestsSuite.js
@@ -1,4 +1,10 @@
+//util tests
 import "./util/chartTest.js"
+import "./util/chartFunctionsTest.js"
 
+//projector tests
+import "./projector/simpleScatterplotChartTest.js"
+
+//demo tests
 import "../../demo/simple-bar-chart/simple-bar-chart-demoTest.js"
 import "../../demo/simple-scatterplot-chart/simple-scatterplot-chart-demoTest.js"
