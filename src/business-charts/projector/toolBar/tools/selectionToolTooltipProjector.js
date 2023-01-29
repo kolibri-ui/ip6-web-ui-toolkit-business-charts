efc9a9ed6456922cd709f49da8445852ab42a1cc
valentina <valentina.giampa@students.fhnw.ch>
2023-01-29 12:17:11 +0100

    feat(tooltip): add tooltip bottom center

--- src/business-charts/projector/toolBar/tools/selectionToolTooltipProjector.js
+++ src/business-charts/projector/toolBar/tools/selectionToolTooltipProjector.js
@@ -14,13 +14,17 @@ const selectionToolBubbleTooltip = (canvasPosition, pointRadius, title) => {
 
 const selectionToolTooltip = (canvasPosition, pointRadius, title) => {
     const tooltipElement = document.createElement('div');
-    tooltipElement.style.top = `${canvasPosition.yValue + 31 - pointRadius}px`;
-    tooltipElement.style.left = `${canvasPosition.xValue -15}px`;
-    tooltipElement.classList.add('tooltip-data', 'tooltip-data-arrow-top-left');
+    tooltipElement.classList.add('tooltip-data', 'tooltip-data-arrow-bottom-center');
+    
+    const tooltipDataRoot = document.querySelector(':root');
+    const computedTooltipWidth = Number(getComputedStyle(tooltipDataRoot).getPropertyValue("--tooltip-width"));
+    const computedArrowBorder = Number(getComputedStyle(tooltipDataRoot).getPropertyValue("--tooltip-arrow-border-width"));
 
+    tooltipElement.style.top = `${canvasPosition.yValue + pointRadius + computedArrowBorder}px`;
+    tooltipElement.style.left = `${canvasPosition.xValue - 0.5*computedTooltipWidth}px`;
+    
     const text = document.createTextNode(title);
     tooltipElement.append(text);
 
     return tooltipElement;
 };
-
