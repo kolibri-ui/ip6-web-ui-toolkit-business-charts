ab87da6d3d3353dac93a8c305a881922a4d567c5
valentina <valentina.giampa@students.fhnw.ch>
2023-01-29 17:23:41 +0100

    refactor: custom properties

--- src/business-charts/projector/toolBar/tools/selectionToolTooltipProjector.js
+++ src/business-charts/projector/toolBar/tools/selectionToolTooltipProjector.js
@@ -2,9 +2,11 @@ export { selectionToolBubbleTooltip, selectionToolTooltipBottomCenter, selection
 
 const selectionToolBubbleTooltip = (canvasPosition, pointRadius, title) => {
     const tooltipElement = document.createElement('div');
+    
     const tooltipDataRoot = document.querySelector(':root');
-    const computedTooltipHeight = Number(getComputedStyle(tooltipDataRoot).getPropertyValue("--tooltip-min-height"));
-    const computedArrowBorder = Number(getComputedStyle(tooltipDataRoot).getPropertyValue("--tooltip-bubble-arrow-border-right"));
+    const computedTooltipHeight = Number(getComputedStyle(tooltipDataRoot).getPropertyValue("--tooltip-min-height").replace("px",""));
+    const computedArrowBorder = Number(getComputedStyle(tooltipDataRoot).getPropertyValue("--tooltip-bubble-arrow-border-right").replace("px",""));
+    
     tooltipElement.style.top = `${canvasPosition.yValue - pointRadius -computedTooltipHeight}px`;
     tooltipElement.style.left = `${canvasPosition.xValue + pointRadius - computedArrowBorder}px`;
     tooltipElement.classList.add('tooltip-bubble', 'tooltip-bubble-bottom-left');
@@ -19,11 +21,11 @@ const selectionToolTooltipBottomCenter = (canvasPosition, pointRadius, title) =>
     const tooltipElement = document.createElement('div');
     
     const tooltipDataRoot = document.querySelector(':root');
-    const computedTooltipWidth = Number(getComputedStyle(tooltipDataRoot).getPropertyValue("--tooltip-width"));
-    const computedArrowBorder = Number(getComputedStyle(tooltipDataRoot).getPropertyValue("--tooltip-arrow-border-width"));
-
+    const computedTooltipWidth = Number(getComputedStyle(tooltipDataRoot).getPropertyValue("--tooltip-width").replace("px",""));
+    const computedArrowBorder = Number(getComputedStyle(tooltipDataRoot).getPropertyValue("--tooltip-arrow-border-width").replace("px",""));
+    
     tooltipElement.style.top = `${canvasPosition.yValue + pointRadius + computedArrowBorder}px`;
-    tooltipElement.style.left = `${canvasPosition.xValue - 0.5*computedTooltipWidth}px`;
+    tooltipElement.style.left = `${canvasPosition.xValue - 0.5 * computedTooltipWidth}px`;
 
     tooltipElement.classList.add('tooltip-data-bottom-center', 'tooltip-data-arrow-top-center');
     
@@ -37,11 +39,11 @@ const selectionToolTooltipLeftCenter = (canvasPosition, pointRadius, title) => {
     const tooltipElement = document.createElement('div');
 
     const tooltipDataRoot = document.querySelector(':root');
-    const computedTooltipWidth = Number(getComputedStyle(tooltipDataRoot).getPropertyValue("--tooltip-width"));
-    const computedTooltipHeight = Number(getComputedStyle(tooltipDataRoot).getPropertyValue("--tooltip-height"));
-    const computedArrowBorder = Number(getComputedStyle(tooltipDataRoot).getPropertyValue("--tooltip-arrow-border-width"));
+    const computedTooltipWidth = Number(getComputedStyle(tooltipDataRoot).getPropertyValue("--tooltip-width").replace("px",""));
+    const computedTooltipHeight = Number(getComputedStyle(tooltipDataRoot).getPropertyValue("--tooltip-height").replace("px",""));
+    const computedArrowBorder = Number(getComputedStyle(tooltipDataRoot).getPropertyValue("--tooltip-arrow-border-width").replace("px",""));
 
-    tooltipElement.style.top = `${canvasPosition.yValue -0.5*computedTooltipHeight}px`;
+    tooltipElement.style.top = `${canvasPosition.yValue - 0.25 * computedTooltipHeight}px`;
     tooltipElement.style.left = `${canvasPosition.xValue - computedTooltipWidth - pointRadius - computedArrowBorder}px`;
 
     tooltipElement.classList.add('tooltip-data-left-center', 'tooltip-data-arrow-right-center');
