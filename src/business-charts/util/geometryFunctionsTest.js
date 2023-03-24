2731a69d751ea6905d865af9b38655c70143ce85
rogerkreienbuehl <r.kreienbuehl@gmx.ch>
2023-03-24 20:12:55 +0100

    test: added missing tests for geometry functions

--- src/business-charts/util/geometryFunctionsTest.js
+++ src/business-charts/util/geometryFunctionsTest.js
@@ -1,40 +1,54 @@
 import { TestSuite } from "../../Kolibri/docs/src/kolibri/util/test.js";
 import {
+    calcXRatio,
+    calcYRatio,
     pointCanvasToDomain,
     pointDomainToCanvas,
     xCanvasToDomain,
     xDomainToCanvas,
     yCanvasToDomain,
     yDomainToCanvas
-}                    from "./geometryFunctions.js"
+} from "./geometryFunctions.js"
 
 const geometryFunctionsTestSuite = TestSuite("src/business-charts/utils/geometryFunctions");
 
-geometryFunctionsTestSuite.add("xDomainToCanvas", assert => {
+geometryFunctionsTestSuite.add("test calcXRatio", assert => {
+    const result = calcXRatio(500, 0, 10);
+
+    assert.is(result, 50);
+});
+
+geometryFunctionsTestSuite.add("test calcYRatio", assert => {
+    const result = calcYRatio(400, 0, 10);
+
+    assert.is(result, 40);
+});
+
+geometryFunctionsTestSuite.add("test xDomainToCanvas", assert => {
     const result = xDomainToCanvas(500, 0, 12, 6);
 
     assert.is(result, 250);
 });
 
-geometryFunctionsTestSuite.add("yDomainToCanvas", assert => {
+geometryFunctionsTestSuite.add("test yDomainToCanvas", assert => {
     const result = yDomainToCanvas(400, 0, 10, 5);
 
     assert.is(result, 200);
 });
 
-geometryFunctionsTestSuite.add("xCanvasToDomain", assert => {
+geometryFunctionsTestSuite.add("test xCanvasToDomain", assert => {
     const result = xCanvasToDomain(500, 0, 12, 250);
 
     assert.is(result, 6);
 });
 
-geometryFunctionsTestSuite.add("yCanvasToDomain", assert => {
+geometryFunctionsTestSuite.add("test yCanvasToDomain", assert => {
     const result = yCanvasToDomain(400, 0, 10, 200);
 
     assert.is(result, 5);
 });
 
-geometryFunctionsTestSuite.add("pointDomainToCanvas", assert => {
+geometryFunctionsTestSuite.add("test pointDomainToCanvas", assert => {
     const result = pointDomainToCanvas(
         500,
         400,
@@ -51,7 +65,7 @@ geometryFunctionsTestSuite.add("pointDomainToCanvas", assert => {
     assert.is(result.yValue, 200);
 });
 
-geometryFunctionsTestSuite.add("pointCanvasToDomain", assert => {
+geometryFunctionsTestSuite.add("test pointCanvasToDomain", assert => {
     const result = pointCanvasToDomain(500, 400, { xValue: 0, yValue: 0 }, { xValue: 12, yValue: 10 }, {
         xValue: 250,
         yValue: 200
